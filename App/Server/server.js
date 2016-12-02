0051c045e33466319bb971590fb27e24014dfee0
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-02 15:42:24 -0800

    merge conflict

--- App/Server/server.js
+++ App/Server/server.js
@@ -1,20 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
-<<<<<<< HEAD
-var router = require('./router.js')
-
-=======
->>>>>>> signin work EL
+var router = require('./router.js');
 
 var app = express();
 var PORT = process.env.PORT || 4321;
 
-<<<<<<< HEAD
 require('./middleware.js')(app, express);
 
 app.use('/', router);
-=======
->>>>>>> signin work EL
 
 app.listen(PORT, function() {
   console.log('Express listening on port', PORT);
