eeeebfbeee7535ef39a48d68f4ad41123e759357
Wasiff <wasiff@gmail.com>
2016-12-05 13:34:21 -0800

    fixed merge conflicts

---
+++ App/Client/login.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Login from './login.vue'
+
+var login = new Vue({
+  el: '.login',
+  components: { Login }
+})
---
+++ App/Client/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <form class="" action="index.html" method="post">
+      <input placeholder="Enter your username here" type="text" v-model="username">
+      <button class="login-button" role="button" href="#" v-on:click.prevent="login"> Log In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: ''
+    }
+  },
+    methods: {
+      login: function() {
+        console.log(`${this.username} is being routed to the video page`)
+        this.$http.get('/api/users/',{username: this.username})
+        .then((res) => {
+          /*do something once we get 200 OK*/
+         })
+        .catch((err)=>console.error(err))
+    },
+  }
+}
+</script>
+
+<style lang="css">
+</style>
