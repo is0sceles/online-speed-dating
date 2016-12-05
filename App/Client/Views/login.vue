e395fa84070a7a50cb1b3be8ab3c4a6b0738f5af
elliottabirch <elliottabirch@gmail.com>
2016-12-05 15:23:24 -0800

    refactored signin controller and template

--- App/Client/Views/login.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="login">
-    <form class="login-form">
-      <input placeholder="Enter your username here" type="text" v-model="username">
-      <button class="login-button" role="button" href="#" @:click.prevent="login"> Log In </button>
-    </form>
-  </div>
-</template>
---
+++ App/Client/Views/signupController.js
@@ -0,0 +1,25 @@
+import temp from './signupTemplate.vue';
+
+var signup = {
+  template: temp.template,
+  data: function () {
+    return {
+      username: ''
+    };
+  },
+  methods: {
+    signup: function ($http) {
+      var body = {
+        username: this.userName,
+      };
+
+      this.$http.post('/api/user', body)
+        .then((response) => {
+          this.$route.router.go('/signin');
+        }, (response) => { });
+      this.$route.router.go('/signin');
+    }
+  }
+};
+
+export default signup;
