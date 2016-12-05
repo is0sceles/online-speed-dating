8a9a6757097f8e6dfad8c1d1f2783059df11c72f
elliottabirch <elliottabirch@gmail.com>
2016-12-05 15:00:56 -0800

    fixe merge conflicts in app.js

--- App/Client/app.js
+++ App/Client/app.js
@@ -39,16 +39,8 @@ const router = new VueRouter({
   routes
 });
 
-<<<<<<< e234507113d5ea7549cbfcc5e052e049e45512e1
-const app = new Vue({
-<<<<<<< 3b5258d3cd194dc49956eabffcad0f0a660ebeed
-  router
-}).$mount('.app');
 
-=======
-=======
 const app = new Vue(
->>>>>>> fixed typo
     router
 }).$mount('.app')
->>>>>>> fixed merge conflict with app.js
+
