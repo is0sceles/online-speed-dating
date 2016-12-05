96af8a2850dde24b39403bb9fa01a96caf210647
elliottabirch <elliottabirch@gmail.com>
2016-12-05 02:25:18 -0800

    set up framework for vuerouter

--- App/Client/app.js
+++ App/Client/app.js
@@ -1,7 +1,33 @@
 import Vue from 'vue';
 import App from './app.vue';
+import VueRouter from 'vue-router';
+import signup from './signup/signup.vue';
+import signin from './signin/signin.vue';
+import video from './video/video.vue';
 
-var app = new Vue({
-  el: '.app',
-  components: { App }
-});
+var routes = [
+    {
+        path: '/',
+        component: App
+    },
+    {
+        path: '/signin',
+        component: siginin
+    },
+    {
+        path: '/video',
+        component: video
+    },
+    {
+        path: '/signup',
+        component: signup
+    }
+]
+
+const router = new VueRouter(
+    routes
+)
+
+consta app = new Vue(
+    router
+).$mount('.app');
