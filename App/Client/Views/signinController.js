1ac143923b2954378bcdc69cc4bd77f41c50c2ab
Dan Gilday <dvgilday@gmail.com>
2016-12-06 14:02:20 -0800

    Update signinController.js

--- App/Client/Views/signinController.js
+++ App/Client/Views/signinController.js
@@ -16,11 +16,7 @@ const Login = {
 
       this.$http.get('/api/user', {params: {username: this.username}})
       .then((res) => { 
-        var phone = window.phone = PHONE({
-          number: res.body.username, // listen on username line else Anonymous
-          publish_key: 'pub-c-97dbae08-7b07-4052-b8e0-aa255720ea8a', // Your Pub Key
-          subscribe_key: 'sub-c-794b9810-b865-11e6-a856-0619f8945a4f'// Your Sub Key
-        }); 
+        
 
 
       })
