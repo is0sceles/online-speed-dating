46bfd7d3932ef5c2680df0cfba09a4f46eb4428e
elliottabirch <elliottabirch@gmail.com>
2016-12-05 19:20:28 -0800

    fixed pathing on test

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,6 +1,6 @@
 
 var express = require('express');
-var router = require('./App/Sever/router.js');
+var router = require('../App/Server/router.js');
 
 var app = express();
 var PORT = process.env.PORT || 4321;
@@ -10,9 +10,9 @@ app.use('/', router);
 
 describe('Server Connection', function(done) {
   it('Should connect successfully to server', function() {
-   app.listen(PORT, function() {
-     console.log('Express listening on port', PORT);
-     done(); 
-   });
- });
+    app.listen(PORT, function() {
+      console.log('Express listening on port', PORT);
+      done(); 
+    });
+  });
 });
