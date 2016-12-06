17dddac662602f7ad3f4fc7b592006ca4bee2943
Dan Gilday <dangitday@gmail.com>
2016-12-06 11:15:57 -0800

    Connect phone on login

--- App/Client/Views/signinController.js
+++ App/Client/Views/signinController.js
@@ -1,4 +1,7 @@
 
+var pubnub = require('pubnub');
+var PHONE = require('node-pubnub-webrtc');
+
 import template from './signinTemplate.vue';
 
 const Login = {
@@ -12,7 +15,12 @@ const Login = {
     login: function() {
 
       this.$http.get('/api/user', {params: {username: this.username}})
-      .then((res) => {
+      .then((res) => { 
+        var phone = window.phone = PHONE({
+          number: res.body.username, // listen on username line else Anonymous
+          publish_key: 'pub-c-97dbae08-7b07-4052-b8e0-aa255720ea8a', // Your Pub Key
+          subscribe_key: 'sub-c-794b9810-b865-11e6-a856-0619f8945a4f'// Your Sub Key
+        }); 
 
 
       })
