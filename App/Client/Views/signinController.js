d9b12c7d94486b2ddd3ab6f6df1c0769761b530c
elliottabirch <elliottabirch@gmail.com>
2016-12-07 01:04:49 -0800

    add auth route for signin

--- App/Client/Views/signinController.js
+++ App/Client/Views/signinController.js
@@ -12,13 +12,11 @@ const Login = {
   methods: {
     login: function() {
 
-      this.$http.get('/api/user', {
-        params: {
-          username: this.username,
-          password: this.password
-        }, 
+      this.$http.post('/auth/signin', {
+        username: this.username,
+        password: this.password 
       })
-      .then((res) => { console.log('request sent'); })
+      .then((res) => { console.log(res); })
       .catch((err) => console.error(err));
     },
   }
