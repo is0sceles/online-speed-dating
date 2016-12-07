9942eebb4ddf6612815d06315e03ca48ce4425f4
elliottabirch <elliottabirch@gmail.com>
2016-12-06 19:40:06 -0800

    configured mlabs DB location

--- App/Database/config.js
+++ App/Database/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoURI = 'mongodb://localhost/speeddatingdb';
+mongoURI = 'mongodb://elliott:e@ds129028.mlab.com:29028/heroku_wsps6bz3';
 mongoose.connect(mongoURI);
 
 var db = mongoose.connection; 
