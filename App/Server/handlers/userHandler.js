3fa46f852ed335616b0d09692d0b6873732acdf5
elliottabirch <elliottabirch@gmail.com>
2016-12-07 01:06:04 -0800

    refactor and add helper functions for auth

--- App/Server/handlers/userHandler.js
+++ App/Server/handlers/userHandler.js
@@ -1,13 +1,16 @@
 var db = require('../../Database/config.js');
 var User = require('../../Database/models/userModel.js');
-
+var passport = require('passport');
 exports.signUpUser = function (req, res) {
   var username = req.body.username;
-  User.findOne({ username: username })
+  var password = req.body.password;
+
+  User.findOne({ username: username, password: password })
     .exec(function (err, user) {
       if (!user) {
         var newUser = new User({
-          username: username
+          username: username,
+          password: password
         });
         newUser.save();
         res.send('user created');
@@ -17,14 +20,27 @@ exports.signUpUser = function (req, res) {
     });
 };
 
-exports.loginUser = function (req, res) {
+exports.getUser = function (req, res) {
   var username = req.query.username;
-  User.findOne({ username: username })
-    .exec(function (err, user) {
-      if (!user) {
-        res.status(404).send('Username does not exist');
-      } else {
-        res.status(200).send(user);
-      }
-    });
+  var password = req.query.password;
+  exports.getUserDB(username, password, function(err, user) {
+    if (err) { console.error(err); }
+    res.status(200).send(user); z;
+  });
+};
+
+exports.authenticate = (
+  passport.authenticate('local'),
+  function(req, res) {
+    console.log('inside authenticate');
+    res.status(200).send(req.user);
+  });
+
+
+exports.getUserDB = function(username, password, cb) {
+  User.findOne({ username: username, password: password })
+  .exec(function(err, user) {
+    if (err) { cb(err); }
+    cb(null, user);
+  });
 };
