3223741711f6ca64e33c211e6f48df85bc9ec99e
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-07 18:38:35 -0800

    Added new data variables to user schema

--- App/Database/schema.js
+++ App/Database/schema.js
@@ -2,8 +2,6 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   username: {type: String, required: true, index: { unique: true } },
-<<<<<<< HEAD
-<<<<<<< HEAD
   password: String,
   userinfo: String,
   name: String,
@@ -11,12 +9,6 @@ var userSchema = mongoose.Schema({
   location: String,
   profileImg: String,
   gender: String
-=======
-  password: String
->>>>>>> parent of 1366b7b... Merge pull request #32 from elau002/working
-=======
-  password: String
->>>>>>> 3f344b6f0bf258e2788b963bc71dba6d1b1b3955
 });
  
 exports.userSchema = userSchema;
