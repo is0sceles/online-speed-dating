83ae0b579145f30ce4402ba12c6ab169276d290b
Eric Lau <ericlau@Corinnas-MacBook-Air.local>
2016-12-08 13:59:22 -0800

    merged button application with dynamic

--- App/Client/Views/profileController.js
+++ App/Client/Views/profileController.js
@@ -6,8 +6,6 @@ var profile = {
   template: temp.template,
   data: function() {
     return {
-<<<<<<< HEAD
-<<<<<<< HEAD
       username: '',
       userinfo: '',
       name: '',
@@ -15,12 +13,7 @@ var profile = {
       location: '',
       profileImg: '',
       gender: ''
-=======
-      verified: this.username === this.$route.params.id
->>>>>>> minor testing on page load
-=======
       verified: false
->>>>>>> Show/hide profile edit button depending on user.
     };
   },
   created () {
@@ -36,54 +29,23 @@ var profile = {
       }
     },
     loadUserProfile: function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
       console.log(this.verified)
       console.log('id:', this.$route.params.id);
-=======
-      console.log(this.$route.params.id);
->>>>>>> Show/hide profile edit button depending on user.
       console.log(this.$store.state.username);
->>>>>>> minor testing on page load
       if (this.$route.params.id !== this.$store.state.username) {
         this.$http.get(
           '/api/user',
           { params: {username: this.$route.params.id}}
         )
-<<<<<<< HEAD
         .then((res)=>{ this.setProfileInfo(res.body); });
       } else { 
         this.setProfileInfo(this.$store.getters.getProfileInfo); 
       } 
-=======
-        .then((res)=>{
-          console.log(res);
-          this.$store.commit('setUser', res.body);
-
-        });
-      } else {
-        this.verified = true;
-                console.log(this.verified);
-      }
-
-    },
-    setUserInfo: function() {
-      
-      this.$store.commit('setUser', {name: 'elliott'});
->>>>>>> Basic Profile Buttons added.
     },
     update: function() {
       this.$router.push('/profileCreate/' + this.username);
     }	
-<<<<<<< HEAD
   },
-=======
-  },``
-  created: function() {
-    this.loadUserProfile();
-  }
->>>>>>> merge issues resolved
 };
 
 export default profile;
