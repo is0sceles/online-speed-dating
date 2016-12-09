3385e43ec01141ade3745be2a49411276b62c554
Wasiff <wasiff@gmail.com>
2016-12-08 16:59:48 -0800

    merge changes

--- App/Server/routes/apiRouter.js
+++ App/Server/routes/apiRouter.js
@@ -4,12 +4,9 @@ var eventHandler = require('../handlers/eventHandler.js');
 
 router.post('/user', userHandler.signUpUser);
 router.get('/user', userHandler.getUser);
-<<<<<<< 29ffebe1b01567c5b1c75c0df51055aa0c271ea0
 router.put('/user', userHandler.updateUser);
-=======
 
 router.get('/events', eventHandler.getEvent);
 
->>>>>>> update to events files, schema, apiRouter & app.js
 
 module.exports = router; 
