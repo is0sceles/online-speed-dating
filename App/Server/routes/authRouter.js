86eebbc927de8132e75ed45bd94815aee0e4feb1
elliottabirch <elliottabirch@gmail.com>
2016-12-09 02:53:49 -0800

    commit changes from origin

--- App/Server/routes/authRouter.js
+++ App/Server/routes/authRouter.js
@@ -6,7 +6,6 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 router.post('/login', passport.authenticate('local'), function(req, res) {
   res.status(200).json(req.user);
 });
-<<<<<<< HEAD
 router.post('/authorize', ensureLoggedIn({ redirectTo: '/error', setReturnTo: false }), function(req, res) {
   console.log('user was authorized');
   res.send(req.user);
@@ -14,10 +13,6 @@ router.post('/authorize', ensureLoggedIn({ redirectTo: '/error', setReturnTo: fa
 router.get('/logout', function(req, res) {
   req.logout();
   res.status(200).send('logged out');
-=======
-router.post('/authorize', ensureLoggedIn('/'), function(req, res) {
-  res.send('user is authorized');
->>>>>>> 9cb194989fab4fb00bdded4fcddeef15f3955997
 });
 
 
