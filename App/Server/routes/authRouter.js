636503e7e8e4a9e4210c1e5dd4437f4a1cf4d22e
elliottabirch <elliottabirch@gmail.com>
2016-12-08 23:20:02 -0800

    add session authorization to server

--- App/Server/routes/authRouter.js
+++ App/Server/routes/authRouter.js
@@ -1,9 +1,13 @@
 var router = require('express').Router();
 var userHandler = require('../handlers/userHandler.js');
 var passport = require('passport');
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.status(200).send(req.user);
+  res.status(200).json(req.user);
+});
+router.post('/authorize', ensureLoggedIn('/'), function(req, res) {
+  res.send('user is authorized');
 });
 
 
