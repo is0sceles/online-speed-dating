cb134415b93df103daad237edb64137f0dd38df9
elliottabirch <elliottabirch@gmail.com>
2016-12-12 13:36:08 -0800

    remove admin zombie code

--- App/Client/Components/adminController.js
+++ App/Client/Components/adminController.js
@@ -89,32 +89,3 @@ var admin = {
 
 
 export default admin;
-
-// data () {
-//     return {
-
-//     };
-//   },
-
-//   methods: {
-//     submit ($http) {
-//       var body = {
-//         username: this.username,
-//         date: this.date,
-//         eventType: this.eventType,
-//         eventName: this.eventName
-//       };
-//       let dbUrl = '/api/events';
-//       this.$http.post(dbUrl, body)
-//       .then((res) => {
-//         //clear form fields
-//         this.username = '',
-//         this.date = '',
-//         this.eventType = '',
-//         this.eventName = '';
-
-//       })
-//       .catch((err) => {
-//         console.error('Something went wrong with POST: ', err);
-//       });
-//     },
